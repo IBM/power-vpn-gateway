98fe5cc95217f53c71352dca876989869051eec4
Travis CI User <travis@example.org>
2023-11-28 21:14:27 +0000

    remove symlinked directories

--- modules/vpc/main.tf
+++
@@ -1,5 +0,0 @@
-resource "ibm_is_vpc" "vpn_vpc" {
-  name                      = var.name
-  resource_group            = var.resource_group_id
-  address_prefix_management = var.address_prefix_management
-}
